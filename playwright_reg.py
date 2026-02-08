a4ce9c79fee51d1ce54021c11e9ac8142d95b7bf
asemeniuc <asemeniuk2010@gmail.com>
2026-02-08 21:38:10 +0300

    Pytest and Playwright integration

--- playwright_reg.py
+++ playwright_reg.py
@@ -28,6 +28,6 @@ with sync_playwright() as playwright:
     context = browser.new_context(storage_state="browser-state.json")
     page = context.new_page()
 
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/dashboard")
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
 
     page.wait_for_timeout(5000)
